2f93ed510c8e750fa0033acfeaf5e8054a9a86b2
Abhishek Tiwari <aureallm2170@gmail.com>
2020-03-08 19:04:29 +0530

    modified files

--- database_based/nominate.c
+++ database_based/nominate.c
@@ -1,35 +1,46 @@
-#include <stdio.h>
-#include <iostream>
-struct Node {
-	struct Node* next;
- 	char* value;
-};
+/*#include <iostream>*/
+#include <my_global.h>
+#include <mysql.h>
 
-class contenders {
-
-	struct Node* head;
-
-public :
-	void nominate(char* value)
-	{
-		struct Node* temp = (struct Node*) malloc(sizeof(struct Node));
-		temp->value = value;
-
-		if (head == NULL)
-		{
-			head = temp;
-		}
-		else
-		{
-			temp->next = head;
-			head = temp;
-		}
-	}
-};
-
+void finish_with_error(MYSQL *con)
+{
+  fprintf(stderr, "%s\n", mysql_error(con));
+  mysql_close(con);
+  exit(1);        
+}
 
 int main(int argc, char **argv)
 {
-	nominate("abhishek");
-	nominate("anuj");
+  if(argv[1] == "")
+  {
+	printf("please provide name in the command line\n"); 
+        return 0;
+  }
+
+  MYSQL *con = mysql_init(NULL);
+  
+  if (con == NULL) 
+  {
+      fprintf(stderr, "%s\n", mysql_error(con));
+      exit(1);
+  }  
+
+  if (mysql_real_connect(con, "localhost", "user12", "34klq*", 
+          "testdb", 0, NULL, 0) == NULL) 
+  {
+      finish_with_error(con);
+  }    
+  
+   char requete[500];
+   memset(requete,'\0',500);
+   strcpy(requete, "INSERT INTO contenders VALUES ('");
+   strcat(requete, argv[1]);
+   strcat(requete, "')");
+   if (mysql_query(con,requete)) {
+      finish_with_error(con);
+  }
+
+  
+  mysql_close(con);
+  exit(0);
 }
